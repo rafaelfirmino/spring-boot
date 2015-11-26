3d03041ba6796d5592d2e63ee64416fca894855b
rgfirmino <rafael.gouveia_@hotmail.com>
2015-11-25 23:22:11 -0200

    SpringBoot

--- src/main/java/br/com/spring/config/JerseyConfig.java
+++ src/main/java/br/com/spring/config/JerseyConfig.java
@@ -1,7 +1,19 @@
 package br.com.spring.config;
 
+import br.com.spring.ws.rest.CityJerseyResource;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.ApplicationPath;
+
 /**
  * Created by rafaelfirmino on 25/11/15.
  */
-public class JerseyConfig {
+@Component
+@ApplicationPath("/jersey")
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        register(CityJerseyResource.class);
+    }
 }
--- src/main/java/br/com/spring/ws/rest/CityJerseyResource.java
+++ src/main/java/br/com/spring/ws/rest/CityJerseyResource.java
@@ -1,7 +1,37 @@
 package br.com.spring.ws.rest;
 
+import br.com.spring.service.FlightService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  * Created by rafaelfirmino on 25/11/15.
  */
+@Component
+@Path("/city")
 public class CityJerseyResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(CityJerseyResource.class);
+
+    @Autowired
+    private FlightService flightService;
+
+    @GET
+    @Path("/search")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response search(@QueryParam("city") String city){
+
+        logger.info("City method started.");
+
+        return Response.ok().entity(flightService.retrieveAirportByCity(city)).build();
+    }
 }
