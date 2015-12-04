4670fc9e2ccde1e5fa78432a56e4d523ace52f9f
rgfirmino <rafael.gouveia_@hotmail.com>
2015-12-03 22:19:29 -0200

    JAVA8

--- src/test/java/Java8Tests.java
+++ src/test/java/Java8Tests.java
@@ -1,5 +1,36 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 /**
  * Created by rafaelfirmino on 03/12/15.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class Java8Tests {
+
+    @Test
+    public void forEachTest(){
+        List<String> list = Arrays.asList("muito grande", "maior", "grande", "tiny");
+        list
+        .stream()
+        .filter(s -> s.startsWith("m"))
+        .map(String::toUpperCase)
+                    .sorted()
+                    .forEach(System.out::println);
+
+        IntStream.range(1, 4)
+                .map(n -> n + 1)
+                .average()
+                .ifPresent(System.out::println);
+
+        list
+                .stream()
+                .map(s -> s.toUpperCase())
+                .reduce((s1, s2) -> s1.length() > s2.length() ? s1 : s2)
+                .ifPresent(System.out::println);
+    }
 }
